a3670b8f19ba7ed5c5652a44dfccc7bac62b85aa
5aymtmy <pg1998ao1015hs920@icloud.com>
2019-08-30 02:53:27 +0900

    ステータスのボタン（だけ）と利用規約のプログラムを他に移してコードをスッキリさせた

--- www/js/app.js
+++ www/js/app.js
@@ -42,59 +42,65 @@ function initMap() {
 
 
 // 現在位置プログラム
-        function getMyPlace() {
-        var output = document.getElementById("result");
-        if (!navigator.geolocation){//Geolocation apiがサポートされていない場合
-          output.innerHTML = "<p>Geolocationはあなたのブラウザーでサポートされておりません</p>";
+  function getMyPlace() {
+
+    var output = document.getElementById("result");
+
+      if (!navigator.geolocation){//Geolocation apiがサポートされていない場合
+        output.innerHTML = "<p>Geolocationはあなたのブラウザーでサポートされておりません</p>";
         return;
       }
-        function success(position) {
-        // var latitude  = position.coords.latitude;//緯度
-        // var longitude = position.coords.longitude;//経度
-        var latitude  = 35.693944
-        var longitude = 139.753611
-        //output.innerHTML = '<p>緯度 ' + latitude + '° <br>経度 ' + longitude + '°</p>';
+      
+    function success(position) {
+      // var latitude  = position.coords.latitude;//緯度
+      // var longitude = position.coords.longitude;//経度
+      var latitude  = 35.693944
+      var longitude = 139.753611
+      //output.innerHTML = '<p>緯度 ' + latitude + '° <br>経度 ' + longitude + '°</p>';
 
-        // 位置情報
-        var latlng = new google.maps.LatLng( latitude , longitude ) ;
-        // マーカーの新規出力
+      // 位置情報
+      var latlng = new google.maps.LatLng( latitude , longitude ) ;
+      // マーカーの新規出力
 
-        if(marker_g){
-          marker_g.setMap(null);
-        }
-         marker_g = new google.maps.Marker( {
-          map: map ,
-          position: latlng ,
-          icon: {
-            url: 'human_pictures/human_black.png',//アイコンのURL
-              anchor: new google.maps.Point(25,25),
-              scaledSize: new google.maps.Size(50, 50) //サイズ
-          }
-
-
-
-        });
-
-        if(circle){
-          circle.setMap(null);
-        }
-         circle = new google.maps.Circle({
-         center: latlng,
-         map: map ,
-         radius: 100 , // 半径（m）
-         fillColor: '#AFDFE7',   // 塗りつぶし色
-         fillOpacity: 0.2,  // 塗りつぶし透過度（0: 透明 ⇔ 1:不透明）
-         strokeColor: '#3333FF',  // 外周色
-         strokeOpacity: 1, // 外周透過度（0: 透明 ⇔ 1:不透明）
-         strokeWeight: 5  // 外周太さ
-        });
-        circle.bindTo("center", marker_g, "position");
-
-
-      };
-      function error() {
-       //エラーの場合
-        output.innerHTML = "座標位置を取得できません";
-      };
-      navigator.geolocation.getCurrentPosition(success, error);//成功と失敗を判断
+      if(marker_g){
+        marker_g.setMap(null);
       }
+
+      marker_g = new google.maps.Marker( {
+        map: map ,
+        position: latlng ,
+        icon: {
+          url: 'human_pictures/human_black.png',//アイコンのURL
+          anchor: new google.maps.Point(25,25),
+          scaledSize: new google.maps.Size(50, 50) //サイズ
+        }
+
+      } );
+
+      if(circle){
+        circle.setMap(null);
+      }
+
+      circle = new google.maps.Circle( {
+        center: latlng,
+        map: map ,
+        radius: 100 , // 半径（m）
+        fillColor: '#AFDFE7',   // 塗りつぶし色
+        fillOpacity: 0.2,  // 塗りつぶし透過度（0: 透明 ⇔ 1:不透明）
+        strokeColor: '#3333FF',  // 外周色
+        strokeOpacity: 1, // 外周透過度（0: 透明 ⇔ 1:不透明）
+        strokeWeight: 5  // 外周太さ
+      } );
+
+      circle.bindTo("center", marker_g, "position");
+
+
+      };
+
+    function error() {
+      //エラーの場合
+      output.innerHTML = "座標位置を取得できません";
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error);//成功と失敗を判断
+}
