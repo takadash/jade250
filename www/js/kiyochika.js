315e85b1516386b2c35eab0f081efcd6b11329c9
snoowty <snoowty7@gmail.com>
2019-09-22 14:12:04 +0900

    グラフの増加率を修正、枠・メーターを試しに追加

--- www/js/kiyochika.js
+++ www/js/kiyochika.js
@@ -136,7 +136,7 @@ function stamp_push5(i) {
       btn_display.insertAdjacentHTML('afterbegin', '<img src="human_pictures/human_red.png">');
       cnt_kiyochika++;
       document.getElementById('cnt_kiyochika').textContent = cnt_kiyochika;
-      document.getElementById('bar5').style.width = cnt_kiyochika + 'px';
+      document.getElementById('bar2').style.width = (cnt_kyoden/64.0*100) + '%';
 
     } else {
       //hyouzi.insertAdjacentHTML('afterbegin', '<b>遠いよ</b>');
