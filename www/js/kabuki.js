990a823bafc4fa3813acbb63dcc1aebc3f5a8799
snoowty <snoowty7@gmail.com>
2019-10-14 13:35:14 +0900

    トーストを自動で消えるやつだけにした

--- www/js/kabuki.js
+++ www/js/kabuki.js
@@ -156,12 +156,12 @@ function stamp_push1(i) {
   function error() {
     //エラーの場合
     // hyouzi.innerHTML = "座標位置を取得できません";
-    alert('座標位置を取得できません');
+    // alert('座標位置を取得できません');
     // '<ons-toast id="myToast" animation="ascend">FABs up!<button onclick="myToast.hide()">ok</button></ons-toast>'
     // toast.insertAdjacentHTML('afterbegin',"ons.notification.toast('Hi there!', { timeout: 1000, animation: 'fall' })");
     // toast.insertAdjacentHTML('afterbegin','<div class="toast"><div class="toast__message">Message Message Message Message Message Message</div><button class="toast__button">ACTION</button></div>');
-    myToast.show();
-    ons.notification.toast('Hi there!', { timeout: 2000, animation: 'lift' })
+    // myToast.show();
+    ons.notification.toast('座標位置を取得できません', { timeout: 2000, animation: 'ascend' })
   };
   navigator.geolocation.getCurrentPosition(Success, error); //成功と失敗を判断
 
