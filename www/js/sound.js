ac67e36bebf0f8573afb65fffe84dd0c076495a0
5aymtmy <pg1998ao1015hs920@icloud.com>
2019-11-22 14:31:00 +0900

    音の関数をapp.jsから新しくsound.jsに移動

---
+++ www/js/sound.js
@@ -0,0 +1,47 @@
+//音源
+var sound = new Audio();
+//document.getElementById('cnt_stamp').textContent = cnt_stamp;
+sound.volume = 0.1;
+function click_sound() {
+  sound.src = "components/decision22.mp3";
+  sound.play();
+};
+
+function get_stamp_sound(){
+  sound.src = "components/decision4.mp3";
+  sound.play();
+};
+
+
+function pin_choice_sound(){
+  sound.src = "components/cursor1.mp3";
+  sound.play();
+
+};
+
+function stamp_failed_sound(){
+  sound.src = "components/blip02.mp3";
+  sound.play();
+};
+
+function menu_open_sound(){
+  sound.src = "components/decision29.mp3";
+  sound.play();
+};
+
+function sound_plus(){
+  if(sound.volume <= 0.9){
+    sound.volume = sound.volume + 0.1;
+    sound.src = "components/decision29.mp3";
+    sound.play();
+    console.log(sound.volume);
+  }
+}
+function sound_minus(){
+  if(sound.volume >= 0.1){
+    sound.volume = sound.volume - 0.1;
+    sound.src = "components/decision29.mp3";
+    sound.play();
+    console.log(sound.volume);
+  } 
+}
